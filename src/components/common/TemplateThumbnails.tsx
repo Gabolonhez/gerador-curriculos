fb178ef858fc4669b5e726c4176158290c20546d
Gabriel Bolonhez Felipe <gbbolonhez@gmail.com>
2025-12-10 11:04:05 -0300

    feat: cleaning and improving architecture

--- src/components/common/TemplateThumbnails.tsx
+++
@@ -1,6 +0,0 @@
-import { TemplateKey } from '../TemplateTypes';
-import ThumbnailMap from './TemplateThumbnailsData';
-
-export default function TemplateThumbnails({ keyName }: { keyName: TemplateKey }) {
-  return <div className="template-thumb" aria-hidden>{ThumbnailMap[keyName]}</div>;
-}
